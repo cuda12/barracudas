a5a9ce5c75427626b3c8520c255380a449d571de
andy <arueesch@gmail.com>
2017-07-20 12:35:14 +0200

    feat: Add prototype for schedule view with dummy data

---
+++ Barracudas/ScheduleViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ScheduleViewController.swift
+//  Barracudas
+//
+//  Created by Andreas Rueesch on 20.07.17.
+//  Copyright © 2017 Andreas Rueesch. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+
+// MARK: - TableView Delegate and DataSource extension
+
+extension ScheduleViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "scheduleNoGamesCell", for: indexPath) 
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "scheduleGamesCell", for: indexPath)
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+}
